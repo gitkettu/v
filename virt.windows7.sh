0987402de073cecc7bec78ace733b7f083c62576
gitkettu <gitkettu@gmail.com>
2022-02-28 22:03:41 +0200

    a

--- virt.windows7.sh
+++
@@ -1,7 +0,0 @@
-virt-install --name=windows7-vm \
---vcpus=2 \
---memory=4096 \
---cdrom=/home/test/Documents/Win7_HomePrem_SP1_English_x64.iso \
---disk size=60 \
---os-variant=win7
-#--check disk_size=off
